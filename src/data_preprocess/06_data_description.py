0f24c1231ebbba48b064853096a4b1df2cadeeec
vallerrr <valler4044@gmail.com>
2024-06-13 18:41:19 +0100

    generating all the description figures for diseases
    2.2 plot: Diseases chapter count by age and gender
    2.3 disease occurrence: within chapter (self-repeating rate)
    2.4 co-occurrence: between diseases (by each chapter)

--- src/data_preprocess/06_data_description.py
+++ src/data_preprocess/06_data_description.py
@@ -4,12 +4,14 @@ Feature: script to generate data descrption for the data
 
 """
 
-import pandas as pd
+
 from src import params
 from src.data_preprocess import utils
 from collections import Counter
-
-from matplotlib import pyplot as plt
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+import pandas as pd
 df_codebook_final = pd.read_csv(params.codebook_path / 'UKB_preprocess_codebook_wave_0_final.csv')
 df = pd.read_pickle(params.final_data_path / 'UKB_wave_0_final_standardised.pkl')
 
@@ -19,6 +21,13 @@ df = pd.read_pickle(params.final_data_path / 'UKB_wave_0_final_standardised.pkl'
 record_column = 'all_icd'
 access_date_column = '53'
 HES_ICD_ids = params.HES_ICD_ids
+level = 'chapter'
+
+# control zone
+chapter_ranges = [x for x in range(1, 16)]+[19]
+disease_column = params.disease_columns_dict[level]
+
+
 # read the single record file and merge the date columns to the single record file
 
 dates_col,df_single_record = utils.import_df_single_record(record_column)
@@ -52,7 +61,7 @@ for column in ['all_icd_first_3', 'icd_parent_coding', 'icd_chapter_coding']:
     df_single_record[f'diseases_within_window_{column}'] = df_single_record.apply(lambda x: x[column][0:x['index_of_first_disease_out_window']] if x['index_of_first_disease_out_window']!=-1 else None, axis=1)
 
 # merge the new three columns back to the original df
-df = pd.merge(df, df_single_record[['eid', 'diseases_within_window_all_icd_first_3', 'diseases_within_window_icd_parent_coding', 'diseases_within_window_icd_chapter_coding']], how='left', left_on='eid', right_on='eid')
+df = pd.merge(df, df_single_record[['eid', 'diseases_within_window_all_icd_first_3', 'diseases_within_window_icd_parent_coding', ]], how='left', left_on='eid', right_on='eid')
 
 for column in ['all_icd_first_3', 'icd_parent_coding', 'icd_chapter_coding']:
     df[f'diseases_within_window_{column}'] = [x if str(x)!='[]' else None for x in  df[f'diseases_within_window_{column}']]
@@ -102,7 +111,7 @@ df_non_standardised.to_pickle(params.final_data_path / 'UKB_wave_0_final_non_sta
 df = pd.read_pickle(params.final_data_path/ 'UKB_wave_0_final_non_standardised.pkl')
 
 # average disease count by age and gender
-import matplotlib.pyplot as plt
+
 df['disease_count'] = [len(x) if str(x) != 'None' else 0 for x in df[f'diseases_within_window_all_icd_first_3']]
 
 
@@ -140,22 +149,19 @@ plt.savefig(params.current_path/'plot/average_disease_count_at_recruitment_all_d
 # ----------------------------------------------------------
 # 2.2 plot: Diseases chapter count by age and gender
 # ----------------------------------------------------------
-
-
-
 def count_chapter(row):
     """
     count the chapter of the diseases for each row in the dataframe
     """
     if str(row) != 'None':
-        chapters = row['diseases_within_window_icd_chapter_coding']
+        chapters = row[disease_column]
         count_dict = Counter(chapters)
     else:
         count_dict = None
     return count_dict
 
 
-df_to_plot = df[['diseases_within_window_icd_chapter_coding', '31', '21022']].copy()
+df_to_plot = df[[disease_column, '31', '21022']].copy()
 # separate diseases by the chapter name (each chapter will have a separate column)
 df_to_plot['chapter_count'] = df_to_plot.apply(count_chapter, axis=1)
 
@@ -199,10 +205,170 @@ ax2.set_title(gender)
 ax2.set_xlabel('Age at recruitment')
 ax2.set_ylabel('Count of Disease Chapters')
 
-handles, labels = ax2.get_legend_handles_labels() # reverse the order of legend
+handles, labels = ax2.get_legend_handles_labels()  # reverse the order of legend
 ax2.legend(reversed(handles), reversed(labels), title='Chapter', title_fontsize='large', fontsize='small', loc='center left', bbox_to_anchor=(1.0, 0.5))
 
 fig.suptitle('Diseases chapter count by age and gender')
 plt.savefig(params.current_path/'plot/diseases_chapter_count_by_age_and_gender.pdf')
+
 #plt.show()
 
+
+# ----------------------------------------------------------
+# 2.3 disease occurrence: within chapter (self-repeating rate)
+# ----------------------------------------------------------
+df = pd.read_pickle(params.final_data_path/ 'UKB_wave_0_final_non_standardised.pkl')
+df_subset = df[['disease_count',disease_column, '31', '21022']].copy()
+
+
+# self-repeating occurrence
+# diseases related vars ['disease_count', 'chapter_1-22']
+
+df_to_plot = df[['31', '21022']+[f'chapter_{x}' for x in range(1, 23)]]
+
+df_to_plot = df_to_plot.pivot_table(index=['31','21022'],values=[f'chapter_{x}' for x in range(1, 23)],aggfunc=np.mean)
+df_to_plot.reset_index(inplace=True)
+df_to_plot.drop(df_to_plot.loc[(df_to_plot['21022'] < 40) | (df_to_plot['21022'] > 70)].index, inplace=True)
+
+# set 31 as categorical variable
+df_to_plot['31'] = df_to_plot['31'].astype('category')
+df_to_plot['31'] = df_to_plot['31'].cat.rename_categories(['Female', 'Male'])
+
+# remove diseases chapters that are bigger than 15
+
+# within disease co-occurrence plot
+fig, (ax1,ax2) = plt.subplots(2, 1)
+plt.rcParams["figure.figsize"] = [10, 20]
+
+gender = 'Male'
+df_subset = df_to_plot[df_to_plot['31'] == gender][['21022'] + [f'chapter_{i}' for i in chapter_ranges]]
+df_subset.set_index('21022', inplace=True)
+df_subset.plot(kind='line',ax=ax1)
+ax1.set_title(gender)
+ax1.set_xlabel('Age at recruitment')
+ax1.set_ylabel('Count of Disease Chapters')
+for column in df_subset.columns:
+    line = ax1.plot(df_subset.index, df_subset[column], label=column)
+    last_x, last_y = df_subset.index[-1], df_subset[column].iloc[-1]
+    ax1.text(last_x, last_y, f'{column}', fontsize=12, verticalalignment='center')
+ax1.legend().set_visible(False)
+
+gender = 'Female'
+df_subset = df_to_plot[df_to_plot['31'] == gender][['21022'] + [f'chapter_{i}' for i in chapter_ranges]]
+df_subset.set_index('21022', inplace=True)
+df_subset.plot(kind='line',  ax=ax2)
+ax2.set_title(gender)
+ax2.set_xlabel('Age at recruitment')
+ax2.set_ylabel('Count of Disease Chapters')
+for column in df_subset.columns:
+    line = ax2.plot(df_subset.index, df_subset[column], label=column)
+    last_x, last_y = df_subset.index[-1], df_subset[column].iloc[-1]
+    ax2.text(last_x, last_y, f'{column}', fontsize=12, verticalalignment='center')
+ax2.legend().set_visible(False)
+
+fig.suptitle('Diseases self-repeating rate by chapter')
+fig.tight_layout(rect=[0, 0.01, 1, 0.98])
+
+plt.savefig(params.current_path/'plot/disease_co_occurrence_within_diseases_by_chapter.pdf')
+
+
+# ----------------------------------------------------------
+# 2.4 co-occurrence: between diseases (by each chapter)
+# ----------------------------------------------------------
+df_subset = df[['disease_count',disease_column, '31', '21022']].copy()
+
+# remove the chapters that we are less interested in
+def remove_chapters_from_list(row,chapter_ranges):
+    """
+    remove the chapters that are not in the chapter_ranges
+    """
+    if str(row) != 'None':
+        chapters = row
+        chapters = [x for x in chapters if x in chapter_ranges]
+    else:
+        chapters = None
+    return chapters
+df_subset[disease_column] = df_subset[disease_column].apply(lambda x: remove_chapters_from_list(x, chapter_ranges))
+# update the diesease count
+df_subset['disease_count']= [len(x) if str(x) != 'None' else 0 for x in df_subset[disease_column]]
+
+
+from mlxtend.preprocessing import TransactionEncoder
+from mlxtend.frequent_patterns import fpgrowth
+
+# Sample data: list of transactions
+dataset = [x for x in df_subset[disease_column].tolist() if str(x)!='None']
+
+# Initialize TransactionEncoder
+te = TransactionEncoder()
+te_ary = te.fit(dataset).transform(dataset)
+
+# Convert to DataFrame for the FP-Growth function
+df_fp = pd.DataFrame(te_ary, columns=te.columns_)
+
+# Using fpgrowth to find frequent itemsets with a minimum support of 0.05
+frequent_itemsets = fpgrowth(df_fp, min_support=0.05, use_colnames=True)
+
+
+
+
+
+fig, (ax1,ax2) = plt.subplots(2,1)
+plt.rcParams["figure.figsize"] = [8,13]
+
+# bar chart of the support of the frequent itemsets
+frequent_itemsets['itemsets_name'] = [', '.join([str(m) for m in list(x)])for x in frequent_itemsets['itemsets']]
+frequent_itemsets_1 = frequent_itemsets.sort_values(by='support')
+ax1.bar(frequent_itemsets_1['itemsets_name'], frequent_itemsets_1['support'], color='skyblue')
+ax1.set_xlabel('Disease Chapter(s)')
+ax1.set_ylabel('Proportion')
+ax1.set_title(' ')
+ax1.set_xticklabels(frequent_itemsets_1['itemsets_name'], rotation=90, fontsize=12)
+# ax1.set_yticklabels(fontsize=14)
+ax1.spines[['right', 'top']].set_visible(False)
+ax1.text(0.2, 1.03, 'a. Proportion of Frequent Itemsets', horizontalalignment='center', verticalalignment='center', transform=ax1.transAxes, fontsize=14)
+# plt.title('Disease co-occurrence between diseases by chapter: bar view')
+
+# network view
+
+frequent_itemsets['itemsets'] = frequent_itemsets['itemsets'].apply(lambda x: list(x))
+
+G = nx.Graph()
+# Adding nodes and edges based on itemsets and their supports
+for _, row in frequent_itemsets.iterrows():
+    items = row['itemsets']
+    if len(items) == 2:
+        G.add_edge(items[0], items[1], weight=row['support'], label=f"{row['support']:.2f}")
+
+# Use a layout that might help reduce edge overlapping
+pos = nx.spring_layout(G, scale=5, seed=42)
+
+# change the pos manually
+
+pos[11][1] = pos[11][1]-2
+pos[11][0] = pos[11][0]+0.5
+pos[9][0] = pos[9][0]+1.5
+pos[10][0] = pos[10][0]-1.3
+pos[10][1] = pos[10][1]-1.5
+pos[19][1] = pos[11][1]
+pos[19][0] = pos[19][0]-1.5
+# Create the plot with specific dimensions
+
+
+# Nodes
+nx.draw_networkx_nodes(G, pos, node_color='skyblue', node_size=800,ax=ax2)
+# Edges
+edges = G.edges(data=True)
+edge_labels = nx.get_edge_attributes(G, 'label')
+nx.draw_networkx_edges(G, pos, edgelist=edges, width=[d['weight']*12 for u, v, d in edges],ax=ax2)
+nx.draw_networkx_labels(G, pos, font_size=12, font_family='sans-serif',ax=ax2)
+nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_color='red', font_size=12,ax=ax2)
+
+ax2.spines[['right', 'top','bottom','left']].set_visible(False)
+# ax2.set_title('Network view of paired chapters')
+ax2.text(0.2, 1, 'b. Network view of paired chapters', horizontalalignment='center', verticalalignment='center', transform=ax2.transAxes, fontsize=14)
+
+fig.suptitle('Disease co-occurrence between diseases by chapter')
+fig.tight_layout(rect=[0, 0.01, 1, 0.98])
+
+plt.savefig(params.current_path/f'plot/disease_co_occurrence_between_diseases_by_{level}.pdf')
